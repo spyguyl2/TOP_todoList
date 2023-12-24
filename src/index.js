eda4453d1fbbeed60c3ab0971f56c2d547d3f948
Damon Haase <damon.haase@gmail.com>
2023-12-24 18:37:08 -0500

    Add filler project for testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './style.css';
-import {createProject, createTodo} from './projectFunctions';
+import './meyerReset.css';
+import {pfx} from './projectFunctions'
+import { startOfToday } from 'date-fns';
 
-let testProject = createProject("Default", "this is the default project");
+let testProject = pfx.createProject("Default", "this is the default project");
+
+
+testProject.addTodo(createTodo('todo1', 'my first todo!', startOfToday(), 'med', true));
+testProject.addTodo(createTodo('todo2', 'my second todo!', startOfToday(), 'high', true));
+testProject.addTodo(createTodo('todo3', 'my third todo!', startOfToday(), 'low', true));
+testProject.addTodo(createTodo('todo4', 'my fourth todo!', startOfToday(), 'low', false));
+testProject.addTodo(createTodo('todo2', 'my fifth todo!', startOfToday(), 'med', true));
+
+
+
+console.log(testProject.getTodos());
+console.log(testProject.getTotalTodos());
+console.log(testProject.getCompleteTodos());
