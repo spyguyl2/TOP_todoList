f3e613b618c3c057d3005957499e76f8f6ead1dd
Damon Haase <damon.haase@gmail.com>
2023-12-24 18:34:50 -0500

    Edit title. Add note ffr

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,33 @@
 const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//add date-fns require's here
 
- module.exports = {
-   entry: {
-     app: './src/index.js',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Production',
-       template: './src/index.html',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|sv|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-   },
- };
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|sv|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+  ],
+  },
+};
