edac8cc562912a60899e56ac954e498aef3f603a
Damon Haase <damon.haase@gmail.com>
2023-12-26 16:12:29 -0500

    Remove uneeded test code. Refactor imports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import './style.css';
 import './meyerReset.css';
-import {pfx} from './projectFunctions'
+import { createIcons, FolderPlus } from 'lucide';
+import { createProject, createSubTask, createTodo } from './projectFunctions';
+import { addProject } from './domkit';
 import { startOfToday } from 'date-fns';
 
-let testProject = pfx.createProject("Default", "this is the default project");
-
-
-testProject.addTodo(createTodo('todo1', 'my first todo!', startOfToday(), 'med', true));
-testProject.addTodo(createTodo('todo2', 'my second todo!', startOfToday(), 'high', true));
-testProject.addTodo(createTodo('todo3', 'my third todo!', startOfToday(), 'low', true));
-testProject.addTodo(createTodo('todo4', 'my fourth todo!', startOfToday(), 'low', false));
-testProject.addTodo(createTodo('todo2', 'my fifth todo!', startOfToday(), 'med', true));
-
-
-
-console.log(testProject.getTodos());
-console.log(testProject.getTotalTodos());
-console.log(testProject.getCompleteTodos());
+createIcons({
+    icons: {
+        FolderPlus
+    }
+});
