16dd27484ca98cdea2fa1df1726605165d56e938
Damon Haase <damon.haase@gmail.com>
2024-01-10 12:19:19 -0500

    Add global variable. Finish displayNewTodo()

--- src/domkit.js
+++ src/domkit.js
@@ -1,3 +1,7 @@
+import { createProject } from "./projectFunctions";
+
+const todoPanel = document.querySelector('.todoPanel');
+
 export const addProject = (Project) => {
     const parent = document.querySelector('.projectPanel');
     const projectPane = document.createElement('div');
@@ -34,41 +38,98 @@ const createInput = (name, type = 'text') => {
     return input;
 }
 
-export const displayNewTodoModal = () => {
+const displayModal = () => {
     const outer = document.createElement('div');
-    outer.id = 'newtodomodal';
     outer.classList.add('modal-outer'); 
+    outer.addEventListener('click', () => {outer.remove();});
 
     const inner = document.createElement('div');
     inner.classList.add('modal-inner');
+    
+    outer.appendChild(inner);
 
     const form = document.createElement('form');
-    form.id = 'newtodoform';
+    inner.appendChild(form);
+
+    return form;
+}
+
+export const displayNewTodoModal = () => {
+
+    const outer = document.createElement('div');
+    todoPanel.appendChild(outer);
+    outer.classList.add('modal-outer');
+    outer.addEventListener('click', (e) => {
+        let isOutside = !e.target.closest('.modal-inner');
+        if (isOutside) outer.remove();
+    });
+
+    const inner = document.createElement('div');
+    inner.classList.add('modal-inner');
+    
+    outer.appendChild(inner);
+
+    const form = document.createElement('form');
+    inner.appendChild(form);
 
     const h1 = createH1('New Todo');
+    form.appendChild(h1);
 
     const labelTitle = createLabel('title');
-    const inputTitle = createInput('title');
+    form.appendChild(labelTitle);
+
+    const inputTitle = createInput('Title');
+    form.appendChild(inputTitle);
 
     const labelDescription = createLabel('description');
-    const inputDescription = createInput('description');
+    form.appendChild(labelDescription);
+
+    const inputDescription = createInput('Description');
+    form.appendChild(inputDescription);
 
     const labelDate = createLabel('Date');
+    form.appendChild(labelDate);
+
     const inputDate = createInput('Date', 'date');
+    form.appendChild(inputDate);
 
     const labelPriority = createLabel('priority');
+    form.appendChild(labelPriority);
+
     const priorityBtns = document.createElement('div');
     priorityBtns.classList.add('prioritybuttons');
+    form.appendChild(priorityBtns);
 
     const btnLow = document.createElement('button');
+    priorityBtns.appendChild(btnLow);
+    btnLow.classList.add('low');
+    btnLow.textContent = 'Low';
     const btnMed = document.createElement('button');
+    priorityBtns.appendChild(btnMed);
+    btnMed.classList.add('med', 'selected');
+    btnMed.textContent = 'Med';
     const btnHigh = document.createElement('button');
+    priorityBtns.appendChild(btnHigh);
+    btnHigh.classList.add('high');
+    btnHigh.textContent = 'High';
+
+    priorityBtns.childNodes.forEach((btn) => btn.setAttribute('type', 'button'));
 
     const btnWrapper = document.createElement('div');
+    form.appendChild(btnWrapper);
     btnWrapper.classList.add('newtodomodalbuttonwrapper');
 
     const btnCancel = document.createElement('button');
+    btnWrapper.appendChild(btnCancel);
+    btnCancel.setAttribute('type', 'button');
+    btnCancel.textContent = 'Cancel'
+    btnCancel.addEventListener('click', () => {outer.remove();});
+
     const btnCreate = document.createElement('button');
+    btnWrapper.appendChild(btnCreate);
+    btnCreate.setAttribute('type', 'button');
+    btnCreate.textContent = 'Create Todo';
+    btnCreate.addEventListener('click', createProject)
 
 }
 
